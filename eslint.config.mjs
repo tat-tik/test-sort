ba6c6b9aeb5c8fe9323e94ad857ef70c35c43743
Tanya <an5227952@gmail.com>
2025-04-30 21:21:02 +0300

    change eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,47 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
 import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
 import jest from "eslint-plugin-jest";
 
-
 export default defineConfig([
-    {
-        languageOptions: { globals: { ...globals.browser, ...globals.node } }
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+      "jest/expect-expect": "error",
     },
-    {
-        // files: ['src/**/*.js'],
-        rules: {
-            indent: [
-                "error", 4
-            ], // отступы, авто
-            semi: [
-                "error", "always"
-            ], // точка с запятой, авто
-            "no-unused-vars": "off", // не испоьзуемые переменные
-            "no-console": "off", // console.log
-            "no-var": "error",
-            quotes: [
-                "error", "double"
-            ],
-        },
-    },
-    {
-        ignores: [
-            "dist/", "*.json"
-        ], // отключение проверок для папок
-    },
-    {
-        files: ["**/*.{js,mjs,cjs}"],
-        plugins: { js },
-        extends: ["js/recommended"]
-    },
-    {
-        files: ["**/*.test.js"],
-        ...jest.configs["flat/recommended"],
-        rules: {
-            ...jest.configs["flat/recommended"].rules,
-            "jest/prefer-expect-assertions": "off",
-            "jest/expect-expect": "error",
-        },
-    },
+  },
+
+  { files: ["/.{js,mjs,cjs}"] },
+  { files: ["**/.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { files: ["*/.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  
 ]);
+
